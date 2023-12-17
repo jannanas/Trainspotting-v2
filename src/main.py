208afe74d1b031d8a39eb6a48b007cb303e4d0b6
Jannes Kelso <J.J.Kelso@student.tudelft.nl>
2023-12-17 17:22:22 +0100

    Save

--- src/main.py
+++ src/main.py
@@ -7,19 +7,14 @@ from datetime import datetime, date
 def setupLogging():
     logging.getLogger("selenium.webdriver.remote.remote_connection").setLevel(logging.ERROR)
     logging.getLogger("urllib3.connectionpool").setLevel(logging.ERROR)
-    logging.basicConfig(level=logging.NOTSET, format='%(asctime)s.%(msecs)03d %(levelname)s -- %(message)s', datefmt='%Y-%m-%d %H:%M:%S', filename=fr'D:\Jannes\Documents\Trainspotting v2\output\log\validation_{datetime.now().strftime("%Y-%m-%d--%H-%M-%S")}.log', filemode='w')
+    logging.basicConfig(level=logging.NOTSET, format='%(asctime)s %(levelname)s -- %(message)s', datefmt='%Y-%m-%d %H:%M:%S', filename=fr'D:\Jannes\Documents\Trainspotting v2\output\log\validation_{datetime.now().strftime("%Y-%m-%d--%H-%M-%S")}.log', filemode='w')
     return logging.getLogger("main")
 
 def main():
     logger = setupLogging()
     logger.info("Started execution")
 
-    # Setup services to inject
-    # Reuse same queryService for queries so new connection doesnt need to be established each time
-    queryService = QueryService(headless=True) 
-    dataExtractorService = DataExtractorService()
-    journeyValidatorService = JourneyValidatorService(queryService, dataExtractorService)
-    
+    journeyValidatorService = JourneyValidatorService()
     journeyValidatorService.validateJourneys(date(2023, 12, 17), date(2023, 12, 18))
     
     logger.info("Finished execution")
